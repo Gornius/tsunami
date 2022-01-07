2e0c65a94a5bec50fb03b1b670488477fedd75b1
Rafał Cisek <rafal.cisek121@gmail.com>
2022-01-07 22:00:47 +0100

    Popular video repository

---
+++ DesktopApp/src/Service/YoutubePopularVideosRepository.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Newtonsoft.Json;
+
+namespace DesktopApp.Service
+{
+    public class YoutubePopularVideosRepository : IVideoRepository
+    
+    {
+        public List<Video> GetVideos() {
+            var apiService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = Environment.GetEnvironmentVariable("YOUTUBEAPIKEY")
+            });
+
+            var request = apiService.Videos.List("snippet");
+            request.Chart = VideosResource.ListRequest.ChartEnum.MostPopular;
+            request.MaxResults = 200;
+            var results = request.Execute();
+            
+            
+            var videoList = new List<Video>();
+            foreach (var video in results.Items)
+            {
+                videoList.Add(new Video()
+                {
+                    CategoryId = video.Id,
+                    Tags = video.Snippet.Tags != null ? new List<string>(video.Snippet.Tags) : new List<string>(),
+                });
+            }
+            
+            return videoList;
+        }
+    }
+}
