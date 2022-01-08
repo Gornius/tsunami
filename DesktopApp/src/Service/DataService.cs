65766d1cf90ccf1af16de2f74a3ce183983b1745
polydome <polydome@outlook.com>
2022-01-08 20:42:43 +0100

    Finish `PopulateCategories` algorithm

--- DesktopApp/src/Service/DataService.cs
+++ DesktopApp/src/Service/DataService.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using DesktopApp.Model;
 
 namespace DesktopApp.Service
 {
@@ -7,17 +10,24 @@ namespace DesktopApp.Service
         private readonly IVideoRepository _videoRepository;
         private readonly ICategoryService _categoryService;
         private readonly ICategoryRepository _categoryRepository;
+        private readonly INewsRepository _newsRepository;
+        private readonly ITrendRepository _trendRepository;
 
-        public DataService(IVideoRepository videoRepository, ICategoryService categoryService, ICategoryRepository categoryRepository)
+        public DataService(IVideoRepository videoRepository, ICategoryService categoryService,
+            ICategoryRepository categoryRepository, INewsRepository newsRepository, ITrendRepository trendRepository)
         {
             _videoRepository = videoRepository;
             _categoryService = categoryService;
             _categoryRepository = categoryRepository;
+            _newsRepository = newsRepository;
+            _trendRepository = trendRepository;
         }
 
         public void PopulateCategories()
         {
-            var nonExistingCategories = _videoRepository.GetVideos()
+            var videos = _videoRepository.GetVideos();
+
+            var nonExistingCategories = videos
                 .Select(video => video.CategoryId)
                 .Distinct()
                 .Where(categoryId => !_categoryRepository.CategoryExists(categoryId));
@@ -26,6 +36,40 @@ namespace DesktopApp.Service
                 .Select(categoryId => _categoryService.FetchCategoryById(categoryId))
                 .ToList()
                 .ForEach(category => _categoryRepository.CreateCategory(category));
+
+            var categoryIdsToCounts = videos
+                .Select(video => video.CategoryId)
+                .Aggregate(new Dictionary<string, int>(), IncrementItemOccurrence);
+
+            var articlesCounts = videos
+                .Select(video => video.CategoryId)
+                .Distinct()
+                .Select(categoryId => _categoryService.FetchCategoryById(categoryId))
+                .Select(category => _newsRepository.GetArticlesCount(new List<string>(new[] {category.Title})));
+
+            categoryIdsToCounts.Zip(articlesCounts)
+                .ToList()
+                .ForEach(entry =>
+                {
+                    var (idToVideosCount, articlesCount) = entry;
+                    var (id, videosCount) = idToVideosCount;
+                    var trend = new Trend {
+                        ArticlesCount = articlesCount,
+                        VideosCount = videosCount,
+                        Date = DateTime.Today
+                    };
+                    _trendRepository.AddCategoryTrend(id, trend);
+                });
+        }
+        
+        private static Dictionary<string, int> IncrementItemOccurrence(Dictionary<string, int> itemsToOccurrences, string item)
+        {
+            if (itemsToOccurrences.ContainsKey(item))
+                itemsToOccurrences.Add(item, 0);
+            else
+                itemsToOccurrences[item] += 1;
+
+            return itemsToOccurrences;
         }
     }
 }
