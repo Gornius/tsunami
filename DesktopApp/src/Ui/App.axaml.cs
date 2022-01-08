be749abe48b341eef72eb183b4ba99db262904d3
polydome <polydome@outlook.com>
2022-01-08 20:03:34 +0100

    Move UI files to separate dir

--- DesktopApp/src/Ui/App.axaml.cs
+++ DesktopApp/src/Ui/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace DesktopApp
+namespace DesktopApp.Ui
 {
     public class App : Application
     {
--- DesktopApp/src/Ui/MainWindow.axaml.cs
+++ DesktopApp/src/Ui/MainWindow.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace DesktopApp
+namespace DesktopApp.Ui
 {
     public partial class MainWindow : Window
     {
