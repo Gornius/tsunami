938c85874975cd07b3aed7a973395e74ab334323
polydome <polydome@outlook.com>
2022-01-08 19:08:26 +0100

    Implement RandomNewsService

---
+++ DesktopApp/src/News/RandomNewsService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesktopApp.News
+{
+    public class RandomNewsService : INewsRepository
+    {
+        private readonly int _minCount;
+        private readonly int _maxCount;
+        private readonly Random _random = new();
+
+        public RandomNewsService(int minCount, int maxCount)
+        {
+            _maxCount = maxCount;
+            _minCount = minCount;
+        }
+
+        public int GetArticlesCount(List<string> keywords)
+        {
+            return _random.Next(_minCount, _maxCount);
+        }
+    }
+}
