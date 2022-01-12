29d97befcd58c4295bc27b47e1e3eef8f2c3591e
polydome <polydome@outlook.com>
2022-01-12 13:37:05 +0100

    Add method for selecting all category trends

--- DesktopApp/src/Service/ITrendRepository.cs
+++ DesktopApp/src/Service/ITrendRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DesktopApp.Model;
 
 namespace DesktopApp.Service
@@ -6,5 +7,6 @@ namespace DesktopApp.Service
     {
         void AddCategoryTrend(string categoryId, Trend trend);
         void AddTagTrend(string tagTitle, Trend trend);
+        List<CategoryTrend> FindAllCategoryTrends();
     }
 }
