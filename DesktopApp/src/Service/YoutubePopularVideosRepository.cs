b8f2315d32b74ee8b961fb5f40f093ccf7577667
polydome <polydome@outlook.com>
2022-01-27 18:38:47 +0100

    Convert to OAuth

--- DesktopApp/src/Service/YoutubePopularVideosRepository.cs
+++ DesktopApp/src/Service/YoutubePopularVideosRepository.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using DesktopApp.Model;
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using Google.Apis.YouTube.v3;
 
 namespace DesktopApp.Service
@@ -9,11 +14,23 @@ namespace DesktopApp.Service
     public class YoutubePopularVideosRepository : IVideoRepository
 
     {
+        private async Task<UserCredential> GetCredential()
+        {
+            await using var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read);
+            
+            return await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                GoogleClientSecrets.Load(stream).Secrets,
+                new[] { YouTubeService.Scope.Youtube },
+                "user", CancellationToken.None, new FileDataStore("Books.ListMyLibrary"));
+        }
+        
         public List<Video> GetVideos()
         {
+            UserCredential credential = GetCredential().Result;
+            
             var apiService = new YouTubeService(new BaseClientService.Initializer()
             {
-                ApiKey = Environment.GetEnvironmentVariable("YOUTUBEAPIKEY")
+                HttpClientInitializer = credential
             });
 
             var request = apiService.Videos.List("snippet");
